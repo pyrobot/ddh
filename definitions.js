67011f6469de0236643be12205541ae9159da4c4
Justin Elden <justin.elden@gmail.com>
2012-12-06 17:58:05 -0500

    update testing definition

--- definitions.js
+++ definitions.js
@@ -1,7 +1,7 @@
 var protocol = require('./protocol');
 
 // testing
-protocol.define('test', [{x: 'ushort'}, {y: 'ushort'}], function (x, y) {
-	//console.log("(test) parameters: %d, %d", x, y);
-	this.echo('durp', 1, 2);
+protocol.define('test', [{x: 'float'}, {y: 'float'}], function (x, y) {
+	console.log("(test) parameters: %d, %d", x, y);
+	//this.echo('durp', 1, 2);
 });
